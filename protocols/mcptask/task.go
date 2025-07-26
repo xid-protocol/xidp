2daa4adae6a6bf6a34398a6c083f3a9b1d51b46a
colin-404 <1712472820@qq.com>
2025-07-26 22:35:01 +0800

    add mcptask step

--- protocols/mcptask/task.go
+++ protocols/mcptask/task.go
@@ -5,53 +5,59 @@ import (
 	"github.com/xid-protocol/xidp/protocols"
 )
 
-type TaskState string
+type State string
 
 const (
-	TaskStateSubmitted     TaskState = "submitted"
-	TaskStateInit          TaskState = "init"
-	TaskStateWorking       TaskState = "working"
-	TaskStateInputRequired TaskState = "input-required"
-	TaskStateCompleted     TaskState = "completed"
-	TaskStateCanceled      TaskState = "canceled"
-	TaskStateFailed        TaskState = "failed"
-	TaskStateRejected      TaskState = "rejected"
-	TaskStateAuthRequired  TaskState = "auth-required"
-	TaskStateUnknown       TaskState = "unknown"
+	StateCreated   State = "created"
+	StateWorking   State = "working"
+	StateCompleted State = "completed"
+	StateCanceled  State = "canceled"
+	StateFailed    State = "failed"
+	StateRejected  State = "rejected"
+	StateUnknown   State = "unknown"
 )
 
-type Task struct {
-	ThreadID string         `json:"threadId,omitempty" bson:"threadId,omitempty"`
-	Status   TaskState      `json:"status" bson:"status"`
-	DataType string         `json:"dataType" bson:"dataType"`
-	Data     map[string]any `json:"payload" bson:"payload"`
-	Result   string         `json:"result" bson:"result"`
-	Error    string         `json:"error" bson:"error"`
+type Step struct {
+	StepID string         `json:"stepId" bson:"stepId"`
+	Status State          `json:"status" bson:"status"`
+	Data   map[string]any `json:"data" bson:"data"`
+	Result string         `json:"result" bson:"result"`
+	Error  string         `json:"error" bson:"error"`
 }
 
-func NewTask() *protocols.XID {
+type Task struct {
+	ThreadID string `json:"threadId,omitempty" bson:"threadId,omitempty"`
+	Status   State  `json:"status" bson:"status"`
+	Steps    []Step `json:"steps" bson:"steps"`
+	Result   string `json:"result" bson:"result"`
+	Error    string `json:"error" bson:"error"`
+}
+
+func CreateTask(steps []Step) *protocols.XID {
 	taskid := common.GenerateId()
 
+	//info
 	info := protocols.NewInfo(taskid, "mcptask")
 
-	//添加必填字段
+	//metadata
 	md := protocols.NewMetadata(
 		protocols.OperationCreate,
 		"/protocols/mcptask",
 		"application/json")
+
+	//payload
 	payload := &Task{
-		Status: TaskStateInit,
+		Status: StateCreated,
+		Steps:  steps,
 	}
 	xid := protocols.NewXID(&info, &md, payload)
 	return xid
 }
 
-func CreateTaskEvent(taskid string, threadId string, dataType string, data map[string]any) *protocols.XID {
+func CreateTaskEvent(taskid string, threadId string, step Step) *protocols.XID {
 	task := &Task{
 		ThreadID: threadId,
-		Status:   TaskStateWorking,
-		DataType: dataType,
-		Data:     data,
+		Steps:    []Step{step},
 	}
 	info := protocols.NewInfo(taskid, "mcptask")
 	metadata := protocols.NewMetadata(protocols.OperationUpdate, "/protocols/mcptask", "application/json")
