19fd13da361c75e93471348342428cbb511ff6c5
colin-404 <1712472820@qq.com>
2025-09-08 07:03:21 +0800

    fix InitWithMongo

--- protocols/aiagent/mongo.go
+++ protocols/aiagent/mongo.go
@@ -1,6 +1,8 @@
 package aiagent
 
 import (
+	"errors"
+
 	"github.com/colin-404/logx"
 	"github.com/xid-protocol/common"
 	"github.com/xid-protocol/xidp/protocols"
@@ -8,24 +10,24 @@ import (
 )
 
 // Input unique Agent name
-func InitWithMongo(collection *mongo.Collection, AgentName string) *protocols.XID[Config] {
+func InitWithMongo(collection *mongo.Collection, AgentName string) (*protocols.XID[Config], error) {
 	//check AgentName is valid
 	xid := common.GenerateXid(AgentName)
 	//check agentID is exist
 	exist := common.CheckXidExistsWithMongo(collection, xid, "/protocols/aiagent")
 	if exist {
-		logx.Errorf("Agent %s already exists", AgentName)
-		return nil
+		logx.Errorf("Agent already exists, %s", AgentName)
+		return nil, errors.New("agent already exists")
 	}
-	agentConfig := Config{
+	config := Config{
 		tools: nil,
 	}
 
 	info := NewInfo(AgentName, "")
 	metadata := NewMetadata(protocols.OperationInit)
-	XID := NewXID(&info, &metadata, agentConfig)
+	XID := NewXID(&info, &metadata, config)
 
-	return XID
+	return XID, nil
 }
 
 func UpdateAIAgentWithMongo(collection *mongo.Collection, AgentName string, config Config) *protocols.XID[Config] {
